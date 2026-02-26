588e83e9c97bd26525c69b368e1d8eb1a07f6b9d
Dmitry <fantom2820281@gmail.com>
2026-02-26 18:36:01 +0300

    Все подготовил пробую запускать apply

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "flow" {
+  type    = string
+  default = "dima"
+}
+
+variable "cloud_id" {
+  type = string
+  default = "b1g8isi2f9nvjnsjt5co"
+}
+variable "folder_id" {
+  type = string
+  default = "b1g8c3dr0g4j4rcnj30o"
+}
